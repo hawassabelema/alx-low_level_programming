e9ed91ca13041845189531f68f6b3f6ea54d65a9
Abraham Eshetu <abrahameshetu03@gmail.com>
2022-06-02 14:14:20 -0700

    revised 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,11 @@
 #include <stdio.h>
-
 /**
- *
- *    * main - print to screen
- *
- *       *
- *
- *          * Return: zero
- *
- *             */
-
+   * main - Entry point
+   *
+   * Return: Always 0 (Success)
+   */
 int main(void)
-
 {
-
-		char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(Mystr);
-
-				return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
